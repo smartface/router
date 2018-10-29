b59bf1badf83942760003b349eff8b9945c75600
Alper Ozisik <alper.ozisik@smartface.io>
2018-10-29 23:33:10 +0300

    Reworked for the params of the NativeRouter

--- src/native/NativeRouter.js
+++ src/native/NativeRouter.js
@@ -35,81 +35,84 @@ const Page = require("sf-core/ui/page");
  * @since 1.0.0
  */
 class NativeRouter extends NativeRouterBase {
-  /**
-   * Create OS specific NativeRouter instance
-   * @static
-   * @param {{ path: string, build: function|null, target:object|null, routes: Array, exact: boolean, isRoot: boolean }} param0
-   */
-  static of({
-    path = "",
-    build = null,
-    routes = [],
-    exact = false,
-    renderer = null,
-    to = null,
-    isRoot = false
-  }) {
-    return new NativeRouter({
-      path,
-      build,
-      routes,
-      exact,
-      to,
-      isRoot,
-      renderer: createRenderer()
-    });
-  }
-
-  /**
-   * @constructor
-   * @param {{renderer: Renderer, path: string, build: function|null, routes: Array, exact: boolean, isRoot: boolean }} param0
-   */
-  constructor({
-    path = "",
-    build = null,
-    routes = [],
-    exact = false,
-    renderer = null,
-    isRoot = false,
-    to = null
-  }) {
-    super({ path, build, routes, exact, isRoot, to });
-
-    this._renderer = renderer;
-    if (isRoot) {
-      this._renderer.setRootController(
-        new Page({ orientation: Page.Orientation.AUTO })
-      );
-      const Renderer = require("./Renderer");
-      Renderer.setasRoot(this._renderer._rootController);
+    /**
+     * Create OS specific NativeRouter instance
+     * @static
+     * @param {object} options - 
+     * @param {string} [options.path=""] - Absolute path of the router
+     * @param {function|null} [options.build=null] - Build method of the route
+     * @param {Route[]|NativeRouter[]|NativeStackRouter[]|BottomTabBarRouter[]} [options.routes=[]] - Routes defined within the router
+     * @param {boolean} [options.exact=false] - Matching should be done by exact match
+     * @param {Renderer} [options.renderer] - Renderer is used to render the routes
+     * @param {string|null} [options.to=null] - Redirection path if target path is this router
+     * @param {boolean} [options.isRoot=false] - Specifies whether this router is the root router
+     * @returns {NativeRouter}
+     */
+    static of({
+        path = "",
+        build = null,
+        routes = [],
+        exact = false,
+        renderer = null,
+        to = null,
+        isRoot = false
+    }) {
+        return new NativeRouter({
+            path,
+            build,
+            routes,
+            exact,
+            to,
+            isRoot,
+            renderer: createRenderer()
+        });
     }
-  }
+    constructor({
+        path = "",
+        build = null,
+        routes = [],
+        exact = false,
+        renderer = null,
+        isRoot = false,
+        to = null
+    }) {
+        super({ path, build, routes, exact, isRoot, to });
 
-  /**
-   *
-   * @param {*} router
-   */
-  addChildRouter(router) {
-    this._renderer.show(router._renderer._rootController);
-  }
-
-  onRouteExit(action) {
-    // if (action === "POP") this._renderer.clear();
-  }
-
-  onRouteMatch(route, match, state, action) {
-    const view = super.onRouteMatch(route, match, state);
-
-    if (!view) return false;
-
-    try {
-      view && this._renderer.show(view);
-    } catch (e) {
-      console.log(e.message + "" + e.stack);
+        this._renderer = renderer;
+        if (isRoot) {
+            this._renderer.setRootController(
+                new Page({ orientation: Page.Orientation.AUTO })
+            );
+            const Renderer = require("./Renderer");
+            Renderer.setasRoot(this._renderer._rootController);
+        }
     }
 
-    return true;
-  }
+    /**
+     *
+     * @param {NativeRouter[]|NativeStackRouter[]|BottomTabBarRouter[]} router
+     */
+    addChildRouter(router) {
+        this._renderer.show(router._renderer._rootController);
+    }
+
+    onRouteExit(action) {
+        // if (action === "POP") this._renderer.clear();
+    }
+
+    onRouteMatch(route, match, state, action) {
+        const view = super.onRouteMatch(route, match, state);
+
+        if (!view) return false;
+
+        try {
+            view && this._renderer.show(view);
+        } catch (e) {
+            console.log(e.message + "" + e.stack);
+        }
+
+        return true;
+    }
 }
 
 module.exports = NativeRouter;
